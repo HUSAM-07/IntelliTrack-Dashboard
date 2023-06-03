b80ceeb64bfab8020950c01af78ff2e38f00e103
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-03 18:46:27 +0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",page_title="IntelliTrack",page_icon="📦")
 
 # Sample data for demonstration
 data = pd.DataFrame({
@@ -14,10 +13,7 @@ data = pd.DataFrame({
 })
 
 def plot_location_graph(data):
-    fig, ax = plt.subplots(figsize=(8, 6))
-    sns.set(style="whitegrid")
-    ax = sns.barplot(x='Location', y='Shipment', data=data)
-    ax.set(xlabel='Location', ylabel='Shipment Count')
+    fig = px.bar(data, x='Location', y='Shipment', labels={'Location': 'Location', 'Shipment': 'Shipment Count'})
     return fig
 
 def main():
@@ -26,7 +22,7 @@ def main():
     # Display location tracking graphs
     st.header('Location Tracking')
     fig = plot_location_graph(data)
-    st.pyplot(fig)
+    st.plotly_chart(fig)
     
     # Display detailed dashboard
     st.header('Detailed Dashboard')
